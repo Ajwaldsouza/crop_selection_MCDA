bbcd2f80227363478be88f45f75b054f3d355683
Ajwaldsouza <ajwaldsouza@gmail.com>
2024-04-03 12:15:07 -0400

    started an R project

--- 1_Crop Scouting/species_scouting.R
+++ 1_Crop Scouting/species_scouting.R
@@ -1,28 +1,32 @@
 install.packages("librarian")
 librarian::shelf(plyr, dplyr, tidyr, tibble, rstudioapi,
-                 stringr,taxize,taxizedb, readr, tidyverse, tidytext, purrr,
-                 update_all = FALSE)
+  stringr, taxize, taxizedb, readr, tidyverse, tidytext, purrr,
+  update_all = FALSE, quiet = TRUE)
 
 # Setting working directory
-setwd(dirname(getActiveDocumentContext()$path))                                 # Set working directory to source file location
-mydir="Raw"                                                                     #Mention the directory (as "mydir") in the W.Dir. containing the datafiles
-myfiles= list.files(path=mydir, pattern="*.txt", full.names=TRUE)               #List all the .csv files in the specified "mydir" and assign it to "myfiles". 
-myfiles                                                                         #Shows all the files present 
+setwd(dirname(getActiveDocumentContext()$path))
+mydir <- "Raw" # Mention the directory (as "mydir") in the W.Dir. containing the datafiles
+myfiles <- list.files(path = mydir, pattern = "*.txt", full.names = TRUE) # List all the .csv files in the specified "mydir" and assign it to "myfiles".
+myfiles # Shows all the files present
 
 
-
+hist(rnorm(100))
 
 
 
 # Load the dataset into R
-bibliometric_data <- ldply(myfiles, read_delim, delim = "\t",             #collectively apply "import function for all the files in the specified directory
-                                 quote = "'", escape_double = FALSE, 
-                                 trim_ws = TRUE)%>%     
-  as_tibble()%>%                                                                #Convert the imported data as a dataframe
-  select(select = c(2, 9))%>%
-  rename(authors = select1,
-         title = select2)%>%
-  mutate(id = row_number())                                                     # Add a row called "id" which is the study id
+bibliometric_data <- ldply(myfiles, read_delim,
+  delim = "\t",                         # collectively apply "import function for all the files in the specified directory
+  quote = "'", escape_double = FALSE,
+  trim_ws = TRUE
+) %>%
+  as_tibble() %>% # Convert the imported data as a dataframe
+  select(select = c(2, 9)) %>%
+  rename(
+    authors = select1,
+    title = select2
+  ) %>%
+  mutate(id = row_number()) # Add a row called "id" which is the study id
 
 
 
@@ -30,73 +34,75 @@ bibliometric_data <- ldply(myfiles, read_delim, delim = "\t",             #colle
 
 scientific_names <- str_extract_all(bibliometric_data$title, "\\b[A-Z][a-z]+ [a-z]+\\b")
 
-#Create a new column 'species' and retain 'id'
+# Create a new column 'species' and retain 'id'
 bibliometric_data$species <- NA
 bibliometric_data$species[!sapply(scientific_names, is.null)] <- sapply(scientific_names, paste, collapse = ", ")
-species_raw <- bibliometric_data[!is.na(bibliometric_data$species), c("id", "species")]%>%separate_rows(species, sep = ",")
+species_raw <- bibliometric_data[!is.na(bibliometric_data$species), c("id", "species")] %>% separate_rows(species, sep = ",")
 
 
 
 
 
 # Words/phrases to exclude
-# This list includes the most common filler words in the identified list 
+# This list includes the most common filler words in the identified list
 # Using this list, all the entries containing these words will be removed
 
-words_to_remove <- c("the", "in", "this", "and", "of", "results", "study", "is", 
-                     "to", "these", "we", "it", "our", "all", "medicinal", "was", 
-                     "present", "addition", "chinese", "for", "species", "plants", 
-                     "were", "aim", "on", "plant", "as", "analysis", "by", "are", 
-                     "findings", "with", "data","among", "rights", "materials", 
-                     "extracts", "most", "test", "medicine", "studies", "a", 
-                     "highest", "extract", "from", "total",  "based", "there",
-                     "acid", "antioxidant", "has", "an", "published", 
-                     "staphylococcus", "family", "also", "conclusion", "further", 
-                     "aureus", "although", "compounds", "phytochemical", 
-                     "ethnopharmacological", "current",  "relevance", 
-                     "escherichia", "have", "coli", "at", "rats",  "research",
-                     "essential", "traditional", "activity", "treatment", 
-                     "chemical",  "showed", "both", "two", "using", "after", 
-                     "main",  "different", "according", "region", "use", "its",
-                     "work", "vitro",  "leaves",  "bacillus", 'molecular', 
-                     "effects", "they", "expression",  "pseudomonas",  "method", 
-                     "due", "three", "their", "herbal", "many", "high",
-                     "western","one", "several", "obtained", "together", 
-                     "samples", "despite", "cell","major", "objective", "some", 
-                     "phenolic", "order", "taken", "leaf",  "effect", "indian",
-                     "maximum", "found", "genus", "china", "content",
-                     "production", "can", "recent", "antimicrobial", "cd",
-                     "natural", "while", "antibacterial", "anti", "compared",
-                     "pharmacological",  "concentrations", "structures", "that",
-                     "african", "aspergillus",  "significant", "herb", "herbs",
-                     "four", "docking", "since", "methanol",  "extraction", 
-                     "purpose", "no", "contrast", "during", "murashige", 
-                     "various",  "india", "levels", "pervious", "potential", 
-                     "root", "water", "identified", "crude", "application", 
-                     "result", "quantitative", "investigation",  "methanolic", 
-                     "such", "used", "oil", "presence", "aqueous", "blot", 
-                     "more", "information", "experimental", "asian", "review",
-                     "arabidopsis", "genes", "ethonobotanical", "here", "new",
-                     "active", "cytotoxicity", "under", "paper", "them",
-                     "inhibition", "protein", "when", "analyses", "people",
-                     "screening", "identification", "cells", "chromatography",
-                     "fusarium", "ayurvedic", "mice", "seeds", "stress",
-                     "methods", "salmonella", "five", "considering", "key",
-                     "province",  "test", "countries", "contents", "ethyl",
-                     "other", "administration", "genetic", "through", "isolated",
-                     "toxicity", "vivo","gene", "concentration", "developed",
-                     "best", "district", "growth", "flavonoids", "or", "acute",
-                     "could", "cancer" , "out", "medicines", "seed", "biological",
-                     "africa", "bioactive", "general", "combination", "knowledge",
-                     "oral", "then", "first", "brazilian", "changes", "comparison",
-                     "diabetes", "terminalia", "alkaloids", "beacuse", "material",
-                     "following", "applications", "particular", "average", 
-                     "compound", "increased", "each", "preliminary", "regarding",
-                     "six", "correlation", "had", "investigated", "european",
-                     "structure", "observed", "system", "design", "food", "fruits",
-                     "korean", "mean", "mass", "show", "collected", "control",
-                     "group", "accumulation", "report", "against", "complete",
-                     "into", "inhibits", "survey", "transcriptome", "via")
+words_to_remove <- c(
+  "the", "in", "this", "and", "of", "results", "study", "is",
+  "to", "these", "we", "it", "our", "all", "medicinal", "was",
+  "present", "addition", "chinese", "for", "species", "plants",
+  "were", "aim", "on", "plant", "as", "analysis", "by", "are",
+  "findings", "with", "data", "among", "rights", "materials",
+  "extracts", "most", "test", "medicine", "studies", "a",
+  "highest", "extract", "from", "total", "based", "there",
+  "acid", "antioxidant", "has", "an", "published",
+  "staphylococcus", "family", "also", "conclusion", "further",
+  "aureus", "although", "compounds", "phytochemical",
+  "ethnopharmacological", "current", "relevance",
+  "escherichia", "have", "coli", "at", "rats", "research",
+  "essential", "traditional", "activity", "treatment",
+  "chemical", "showed", "both", "two", "using", "after",
+  "main", "different", "according", "region", "use", "its",
+  "work", "vitro", "leaves", "bacillus", "molecular",
+  "effects", "they", "expression", "pseudomonas", "method",
+  "due", "three", "their", "herbal", "many", "high",
+  "western", "one", "several", "obtained", "together",
+  "samples", "despite", "cell", "major", "objective", "some",
+  "phenolic", "order", "taken", "leaf", "effect", "indian",
+  "maximum", "found", "genus", "china", "content",
+  "production", "can", "recent", "antimicrobial", "cd",
+  "natural", "while", "antibacterial", "anti", "compared",
+  "pharmacological", "concentrations", "structures", "that",
+  "african", "aspergillus", "significant", "herb", "herbs",
+  "four", "docking", "since", "methanol", "extraction",
+  "purpose", "no", "contrast", "during", "murashige",
+  "various", "india", "levels", "pervious", "potential",
+  "root", "water", "identified", "crude", "application",
+  "result", "quantitative", "investigation", "methanolic",
+  "such", "used", "oil", "presence", "aqueous", "blot",
+  "more", "information", "experimental", "asian", "review",
+  "arabidopsis", "genes", "ethonobotanical", "here", "new",
+  "active", "cytotoxicity", "under", "paper", "them",
+  "inhibition", "protein", "when", "analyses", "people",
+  "screening", "identification", "cells", "chromatography",
+  "fusarium", "ayurvedic", "mice", "seeds", "stress",
+  "methods", "salmonella", "five", "considering", "key",
+  "province", "test", "countries", "contents", "ethyl",
+  "other", "administration", "genetic", "through", "isolated",
+  "toxicity", "vivo", "gene", "concentration", "developed",
+  "best", "district", "growth", "flavonoids", "or", "acute",
+  "could", "cancer", "out", "medicines", "seed", "biological",
+  "africa", "bioactive", "general", "combination", "knowledge",
+  "oral", "then", "first", "brazilian", "changes", "comparison",
+  "diabetes", "terminalia", "alkaloids", "beacuse", "material",
+  "following", "applications", "particular", "average",
+  "compound", "increased", "each", "preliminary", "regarding",
+  "six", "correlation", "had", "investigated", "european",
+  "structure", "observed", "system", "design", "food", "fruits",
+  "korean", "mean", "mass", "show", "collected", "control",
+  "group", "accumulation", "report", "against", "complete",
+  "into", "inhibits", "survey", "transcriptome", "via"
+)
 
 
 
@@ -107,7 +113,7 @@ pattern <- paste0("\\b", paste(words_to_remove, collapse = "\\b|\\b"), "\\b")
 
 # Remove rows containing the specified words as standalone words
 
-species_clean <- species_raw[!grepl(pattern, species_raw$species, ignore.case = TRUE), ]%>%
+species_clean <- species_raw[!grepl(pattern, species_raw$species, ignore.case = TRUE), ] %>%
   unique()
 
 
@@ -116,10 +122,10 @@ species_clean <- species_raw[!grepl(pattern, species_raw$species, ignore.case = 
 
 
 # Identify and standardize the scientific names using the NCBI database
-species_identified <- species_clean %>% 
-  mutate(uid = get_uid(species, db = "ncbi", ask = T))%>%
-  na.omit()%>%
-  mutate(sci_name = taxid2name(uid, db = "ncbi", verbose = TRUE, warn = TRUE))%>%
+species_identified <- species_clean %>%
+  mutate(uid = get_uid(species, db = "ncbi", ask = T)) %>%
+  na.omit() %>%
+  mutate(sci_name = taxid2name(uid, db = "ncbi", verbose = TRUE, warn = TRUE)) %>%
   select(-c(2))
 
 
@@ -129,13 +135,16 @@ species_identified <- species_clean %>%
 
 
 
-#_______
+# _______
 
 # FILTERING ONLY THE PLANT KINGDOM
 
-# Use the `classification` function from the 'taxize' package to retrieve the classification
-classification <- taxize::classification(species_identified$sci_name, db = "ncbi", get = "all")
-  
+## Use the `classification` function from the 'taxize' package to retrieve the classification
+classification <-
+taxize::classification(
+  species_identified$sci_name, db = "ncbi", get = "all"
+  )
+
 
 
 # Extract the kingdom for each species from the classification list
@@ -159,7 +168,7 @@ species_plant <- subset(species_identified, kingdom == 33090)
 
 
 
-#____
+# ____
 
 
 
@@ -173,14 +182,14 @@ species_kingdom_plant <- read_csv("species_kingdom_plant.csv")
 # Total study counts of each species
 frequency_counts <- species_kingdom_plant %>%
   group_by(sci_name) %>%
-  summarise(count = n())%>%
+  summarise(count = n()) %>%
   arrange(desc(count))
 
 
 write.csv(frequency_counts, "species_count.csv")
 
 
-#______
+# ______
 
 
 
@@ -189,7 +198,7 @@ write.csv(frequency_counts, "species_count.csv")
 
 
 
-# Supplementary script used to identify the most common words for elimination 
+# Supplementary script used to identify the most common words for elimination
 df_tokens <- species_clean %>%
   unnest_tokens(word, species)
 
@@ -204,7 +213,3 @@ word_list <- word_counts %>%
 write_csv(word_list, "commonwords.csv")
 
 #----
-  
-  
-
-  
